550fbfe7972128309650b44cd4fc6d3056aa70ae
Aldra1n <lks.kramar@proton.me>
2026-05-02 13:11:02 +0200

    fixed armor + correctly corted spells

--- src/spells-main.ts
+++ src/spells-main.ts
@@ -1,7 +1,7 @@
-import type { SimpleItem } from "./types.js";
-import { createSimpleChecklist } from "./simple-checklist.js";
 import { STORAGE_KEY_SPELLS } from "./constants.js";
 import spellsData from "./data/spells.json";
+import { createSimpleChecklist } from "./simple-checklist.js";
+import type { SimpleItem } from "./types.js";
 
 const page = createSimpleChecklist(spellsData as SimpleItem[], STORAGE_KEY_SPELLS);
 page.init();
